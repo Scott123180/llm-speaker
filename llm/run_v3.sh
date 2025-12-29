9e14265cbed4eefd6b2e7336585732fdfdd4c6d5
scott123180 <me@scotthansen.io>
2025-12-28 20:39:37 -0500

    script to run in virtual terminal

---
+++ llm/run_v3.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python3 batch_cleanup.py \
+ --input-dir /home/biosdaddy/Documents/talks/small/small/ \
+ --output-dir /home/biosdaddy/Documents/talks/local_output_v3/ \
+ --model llama8-cleanup-v3
